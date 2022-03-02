b136d354705aa2029bc43d1d25ba74bd89c0e1c9
bruhtazor <74324096+bruhtazor@users.noreply.github.com>
2022-03-02 23:49:39 +0100

    added functions in function.py and modified main.py
    added 2 functions to functions.py, can now acces and use the info in data.json

--- functions.py
+++ functions.py
@@ -1,4 +1,5 @@
 import requests
+from datetime import datetime
 
 #unit = metric(C), imperial(F) or standard(kelvin)
 def get_city_info(api_key, city, unit):
@@ -6,10 +7,28 @@ def get_city_info(api_key, city, unit):
     response = requests.get(url).json()
     return response
 
+#gets temp, max & min temp
 def get_temp(response):
     temp = response['main']['temp']
     tempMin = response['main']['temp_min']
     tempMax = response['main']['temp_max']
     feelsLike = response['main']["feels_like"]
     all = [temp, feelsLike, tempMin, tempMax]
+    return all
+
+#gets weather description
+def get_weather(response):
+    weathDescription = response['weather'][0]['description']
+    return weathDescription
+
+#gets sunrise, sunset in unix timestamp convert to readable hour
+# returns sunrise hour & total sun hour 
+def get_sun_hour(response):
+    sunriseUnix = response['sys']['sunrise']
+    sunrise = datetime.utcfromtimestamp(sunriseUnix).strftime('%H:%M:%S')
+    sunsetUnix = response['sys']['sunset']
+    sunset = datetime.utcfromtimestamp(sunsetUnix).strftime('%H:%M:%S')
+    sunHourUnix = sunsetUnix - sunriseUnix
+    sunHour = datetime.utcfromtimestamp(sunHourUnix).strftime('%H:%M:%S')
+    all = [sunrise, sunset, sunHour]
     return all
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import weatherApp.functions as func
+import functions as func
 from configparser import ConfigParser
 import json
+import time
 
 #get elements from config.ini file
 file = "config.ini"
@@ -9,13 +10,24 @@ config.read(file)
 
 API_KEY = config["Settings"]["apiKey"]
 UNIT = config["Settings"]["unit"]
+city = "lausanne"
 
-#TODO: if else statement with counter to call the api only once per Hour
+'''#counter that calls the api only once per Hour & overwrite the value in data.json
+startTime = time.time()
+while True:'''
 #store data into json file to minimise calling the API
-response = func.get_city_info(API_KEY,"lausanne", UNIT)
+response = func.get_city_info(API_KEY, city, UNIT)
 with open("data.json","w") as outputFile:
     json.dump(response, outputFile)
 outputFile.close()
+    #time.sleep(3600 - ((time.time() - startTime) % 3600))
 
-data = open("data.json", "r")
-print(data.read(5))
+with open("data.json") as dataJson:
+    data = json.load(dataJson)
+temp = func.get_temp(data)
+weather = func.get_weather(data)
+sunHour = func.get_sun_hour(data)
+
+print(f"The actual temperature of {city} is {temp[0]}C that feels like {temp[1]}, with a minimum of {temp[2]}C and a maximum of {temp[3]}C")
+print(f"The weather is {weather}")
+print(f"the sunrises at {sunHour[0]}, sets at {sunHour[1]}, with a total sun hour of {sunHour[2]}")
