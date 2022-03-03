2162d7eb9729357dc6747f3957b933fcef516a7d
bruhtazor <74324096+bruhtazor@users.noreply.github.com>
2022-03-03 22:32:52 +0100

    modified main.py file

--- main.py
+++ main.py
@@ -15,12 +15,16 @@ city = input("enter city name: ")
 response = func.get_city_info(API_KEY, city, UNIT)
 func.overwrite_data(response, "data.json")
 
-with open("data.json") as dataJson:
-    data = json.load(dataJson)
-temp = func.get_temp(data)
-weather = func.get_weather(data)
-sunHour = func.get_sun_hour(data)
+#app loop
+runApp = True
+while runApp:
+    with open("data.json") as dataJson:
+        data = json.load(dataJson)
+    temp = func.get_temp(data)
+    weather = func.get_weather(data)
+    sunHour = func.get_sun_hour(data)
 
-print(f"The actual temperature of {city} is {temp[0]}C that feels like {temp[1]}, with a minimum of {temp[2]}C and a maximum of {temp[3]}C")
-print(f"The weather is {weather}")
-print(f"the sunrises at {sunHour[0]}, sets at {sunHour[1]}, with a total sun hour of {sunHour[2]}")
+    print(f"The actual temperature of {city} is {temp[0]}C that feels like {temp[1]}, with a minimum of {temp[2]}C and a maximum of {temp[3]}C")
+    print(f"The weather is {weather}")
+    print(f"the sunrises at {sunHour[0]}, sets at {sunHour[1]}, with a total sun hour of {sunHour[2]}")
+    
